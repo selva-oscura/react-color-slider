ec5dbb6dd6e7c652e2b07ff3ffe222de4284ae23
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-08 15:19:45 -0800

    checked that App renders with shallow after addition of enzyme to project

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+
+
+it('renders App without crashing (deep)', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+it('renders App without crashing (shallow)', () => {
+  shallow(<App />);
+});
+
+
