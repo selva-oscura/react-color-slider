cfd730d0c83b922e420b8fd61b93b4892d37c8da
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-08 19:52:03 -0800

    added saturation, lightness, alpha to parameters passed from App to ColorSwatch Components

--- src/ColorSwatches.js
+++ src/ColorSwatches.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ColorSwatch from './ColorSwatch';
 
-const ColorSwatches = ({hue}) => (
+const ColorSwatches = ({hue, saturation, lightness, alpha}) => (
 	<div className="ColorSwatches">
 		<ColorSwatch
 			hue={hue}
+			saturation="50"
+			lightness="50"
+			alpha="1"
 			key="1"
 		/>
 		<ColorSwatch
 			hue={hue}
+			saturation={saturation}
+			lightness={lightness}
+			alpha={alpha}
 			key="2"
 		/>
 	</div>
