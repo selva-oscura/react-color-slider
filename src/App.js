3b7a3099d6468bb55e0b22ab98c7f4a04aee7bd1
Carol St.Louis <1.selva.oscura@gmail.com>
2017-01-17 22:51:46 -0800

    moved hue, saturation, lightness, and alpha into a color key in the state object

--- src/App.js
+++ src/App.js
@@ -7,32 +7,34 @@ import ColorSwatches from './ColorSwatches';
 const App = React.createClass({
   getInitialState(){
     return{
-      hue: 0,
-      saturation: 50,
-      lightness: 50,
-      alpha: .5,
+      color: {      
+        hue: 0,
+        saturation: 50,
+        lightness: 50,
+        alpha: .5,
+      }
     }
   },
   updateColour(e){
-    let state = this.state;
-    state[e.target.id] = e.target.value;
-    this.setState(state);
+    let color = this.state.color;
+    color[e.target.id] = e.target.value;
+    this.setState(color);
   },
   render() {
     return (
       <div className="App">
         <Header />
         <ColorSwatches 
-          hue={this.state.hue}
-          saturation={this.state.saturation}
-          lightness={this.state.lightness}
-          alpha={this.state.alpha}
+          hue={this.state.color.hue}
+          saturation={this.state.color.saturation}
+          lightness={this.state.color.lightness}
+          alpha={this.state.color.alpha}
         />
         <ColorRanges 
-          hue={this.state.hue}
-          saturation={this.state.saturation}
-          lightness={this.state.lightness}
-          alpha={this.state.alpha}
+          hue={this.state.color.hue}
+          saturation={this.state.color.saturation}
+          lightness={this.state.color.lightness}
+          alpha={this.state.color.alpha}
           updateColour={this.updateColour}
         />
       </div>
