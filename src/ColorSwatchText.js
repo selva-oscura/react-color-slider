5f657538558e1d2e62d5484d3103179dea3836d9
Carol St.Louis <1.selva.oscura@gmail.com>
2017-06-01 14:47:31 -0700

    removed duplicate code (backgroundColor); reduced indentation;

--- src/ColorSwatchText.js
+++ src/ColorSwatchText.js
@@ -1,17 +1,13 @@
 import React from 'react';
-// import './ColorSwatchText.css';
 
 const ColorSwatchText = ({hue, saturation, lightness, alpha, text}) => {
-	let background = {
-		backgroundColor: "hsla("+hue+", " + saturation + "%, " + lightness + "%, " + alpha + ")",
-	};
 	return(
-			<div className="ColorSwatchText">
-				<p>
-					{text}<br />
-					HSLA: ({hue}, {saturation}%, {lightness}%, {alpha})
-				</p>
-			</div>
+		<div className="ColorSwatchText">
+			<p>
+				{text}<br />
+				HSLA: ({hue}, {saturation}%, {lightness}%, {alpha})
+			</p>
+		</div>
 	)
 };
 
