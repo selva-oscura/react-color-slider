1d24ff5a85e8d151e30783cd4c5a85afcdb605b0
Carol St.Louis <1.selva.oscura@gmail.com>
2017-06-01 11:48:29 -0700

    created wrapper for ColorSwatch and ColorSwatchText components

---
+++ src/ColorSwatchArea.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ColorSwatch from './ColorSwatch';
+import ColorSwatchText from './ColorSwatchText';
+import './ColorSwatchArea.css';
+
+const ColorSwatchArea = ({hue, saturation, lightness, alpha, text}) => {
+	let background = {
+		backgroundColor: "hsla("+hue+", " + saturation + "%, " + lightness + "%, " + alpha + ")",
+	};
+	return(
+		<div className="ColorSwatchArea">
+			<ColorSwatch
+				hue={hue}
+				saturation={saturation}
+				lightness={lightness}
+				alpha={alpha}
+			/>
+			<ColorSwatchText
+				hue={hue}
+				saturation={saturation}
+				lightness={lightness}
+				alpha={alpha}
+				text={text}
+			/>
+		</div>
+	)
+};
+
+export default ColorSwatchArea;
